a5952d4d835438f11e8cdbce0c112da61e6d9c13
Chloe Duan <chloe.duan@hotmail.com>
2017-02-06 19:15:44 -0500

    manhattan distance done , added 2 functions for distance and closest distance cost

--- A1/solution.py
+++ A1/solution.py
@@ -11,6 +11,10 @@ import os
 from search import * #for search engines
 from sokoban import SokobanState, Direction, PROBLEMS, sokoban_goal_state #for Sokoban specific classes and problems
 
+import numpy as np
+
+
+  
 #SOKOBAN HEURISTICS
 def heur_displaced(state):
   '''trivial admissible sokoban heuristic'''
@@ -22,24 +26,45 @@ def heur_displaced(state):
       count += 1
   return count
 
-def heur_manhattan_distance(state):
-#IMPLEMENT
-    '''admissible sokoban heuristic: manhattan distance'''
-    '''INPUT: a sokoban state'''
-    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''      
-    #Admissible heuristic <=> optimistic heuristic. 
-    #Sum Mandhattan Distance - always underestimates cost to get from current state -> goal.
+def distance(coord1, coord2):
+  return abs(coord1[0]-coord2[0]) + abs(coord1[1]-coord2[1]) 
 
-    # Distance Calculation Assumptions:
-    #   - no obstacles on grid 
-    #   - several boxes can fit in one storage bin
-    #Implement this heuristic exactly, even if it is tempting to improve it.
+def closest_storage_cost(coord, coords):
+  distances = (np.abs(coords[:,0]-coord[0])+np.abs(coords[:,1]-coord[1]))
+  return distances[np.argmin(distances)]
     
-    #Return a numeric value - estimate of distance to goal
-    return 0
+    
+def heur_manhattan_distance(state):
+  '''
+  INPUT: sokoban state
+  OUTPUT: numeric value - distance estimate of current to goal state
+
+  Used to estimate how many moves a current state is from a goal state. 
+  The Manhattan distance between coordinates (x0,y0) and (x1,y1) is |x0 - x1| + |y0 - y1|. 
+
+  State Manhattan distance = sum of Manhattan distances btwn each [unstored box & the nearest storage point]
+                  * permitted by the storage restrictions
+                  * ignore obstacle positions 
+                  * assume multiple boxes can be stored at one storage loccation
+  - optimistic heuristic - always underestimates cost 
+  '''
+  
+  box_coords = np.array(list(state.boxes.keys()))
+  box_indices = np.array(list(state.boxes.values()))
+  
+  
+  dist = 0
+  for box in state.boxes:
+    if box not in state.storage:
+      if not state.restrictions:
+        dist += closest_storage_cost(box, np.array(list(state.storage.keys())))
+      else:
+        dist += closest_storage_cost(box, np.array([tuple(c)[0] for c in state.restrictions]))
+  return dist
+
 
 def heur_alternate(state):
-#IMPLEMENT
+  #IMPLEMENT
     '''a better sokoban heuristic'''
     '''INPUT: a sokoban state'''
     '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''         
@@ -50,7 +75,7 @@ def heur_alternate(state):
     return 0
 
 def fval_function(sN, weight):
-#IMPLEMENT
+    #IMPLEMENT
     """
     Provide a custom formula for f-value computation for Anytime Weighted A star.
     Returns the fval of the state contained in the sNode.
@@ -64,18 +89,38 @@ def fval_function(sN, weight):
     #   For UCS, fvalue = gval of the state. 
     #   For best-first search, fvalue = hval of the state.
     # Use this function to create alternate f-value for states; 
-    #               *must be a function of the state and the weight.
+    #               *must be a function of the state and the weight
     
     #Returns numeric f-value - determines state's position on Frontier list in 'custom' search
     
-    #Must initialize search engine object as 'custom' if you supply a custom fval function.
+    #Must initialize search engine object as 'custom' if you supply a custom fval function
+
+
     return 0
 
 def anytime_gbfs(initial_state, heur_fn, timebound = 10):
-#IMPLEMENT
-    '''Provides an implementation of anytime greedy best-first search, as described in the HW1 handout'''
-    '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
-    '''OUTPUT: A goal state (if a goal is found), else False''' 
+    '''
+    INPUT: sokoban state representing start state & timebound (number of seconds)
+	OUTPUT: goal state (if a goal is found), else False'''
+	''' 
+	Anytime Greedy Best-First Search
+	- first expands nodes with lowest h(n) 
+	- may not be optimal
+	- continues searching after a solution is found in order to improve solution quality
+	- Since found a path to the goal after the first iteration, 
+			cost bound for pruning: 
+				if node has g(node) > best path the goal found so far:
+					prune it
+	- algorithm returns when:
+		1. expanded all non-pruned nodes => optimal solution found
+		2. time out
+
+	- prune based on the g-value of the node only because greedy best-first search is not necessarily run with an admissible heuristic
+	
+	Record the time when anytime gb f s is called with os.times()[0]. 
+	Each time you call search, update the time bound with the remaining allowed time. 
+    ''' 
+    OPEN = Open(_BEST_FIRST)
     return False
 
 def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound = 10):
@@ -83,7 +128,32 @@ def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound = 10):
     '''Provides an implementation of anytime weighted a-star, as described in the HW1 handout'''
     '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
     '''OUTPUT: A goal state (if a goal is found), else False''' 
-    return False
+    # return False
+    # OPEN = Open(_ASTAR) #   OPEN = priority queue containing START
+    # CLOSED = []
+
+   ##   current = OPEN.extract
+    # CLOSED.append(current)
+    # while not sokoban_goal_state(current): # while lowest rank in OPEN is not the GOAL:
+    #   current = OPEN.extract # remove lowest rank item from OPEN
+    #   CLOSED.append(current) #   add current to CLOSED
+    #   for 
+    
+
+    #   for neighbors of current:
+    #     cost = g(current) + movementcost(current, neighbor)
+    #     if neighbor in OPEN and cost less than g(neighbor):
+    #       remove neighbor from OPEN, because new path is better
+    #     if neighbor in CLOSED and cost less than g(neighbor):
+    #       remove neighbor from CLOSED
+    #     if neighbor not in OPEN and neighbor not in CLOSED:
+    #       set g(neighbor) to cost
+    #       add neighbor to OPEN
+    #       set priority queue rank to g(neighbor) + h(neighbor)
+    #       set neighbor's parent to current
+
+    # reconstruct reverse path from goal to start
+    # by following parent pointers
 
 if __name__ == "__main__":
   #TEST CODE
@@ -99,7 +169,7 @@ if __name__ == "__main__":
     s0 = PROBLEMS[i] #Problems will get harder as i gets bigger
 
     se = SearchEngine('astar', 'full')
-    se.init_search(s0, goal_fn=sokoban_goal_state, heur_fn=heur_displaced)
+    se.init_search(s0, goal_fn=sokoban_goal_state, heur_fn=heur_manhattan_distance)
     final = se.search(timebound)
 
     if final:
@@ -117,31 +187,31 @@ if __name__ == "__main__":
   print("Problems that remain unsolved in the set are Problems: {}".format(unsolved))      
   print("*************************************") 
 
-  solved = 0; unsolved = []; counter = 0; percent = 0; timebound = 8; #8 second time limit 
-  print("Running Anytime Weighted A-star")   
+  # solved = 0; unsolved = []; counter = 0; percent = 0; timebound = 8; #8 second time limit 
+  # print("Running Anytime Weighted A-star")   
 
-  for i in range(0, 10):
-    print("*************************************")  
-    print("PROBLEM {}".format(i))
+ ##   for i in range(0, 10):
+  #   print("*************************************")  
+  #   print("PROBLEM {}".format(i))
 
-    s0 = PROBLEMS[i] #Problems get harder as i gets bigger
-    weight = 10
-    final = anytime_weighted_astar(s0, heur_fn=heur_displaced, weight=weight, timebound=timebound)
+ ##     s0 = PROBLEMS[i] #Problems get harder as i gets bigger
+  #   weight = 10
+  #   final = anytime_weighted_astar(s0, heur_fn=heur_displaced, weight=weight, timebound=timebound)
 
-    if final:
-      final.print_path()   
-      solved += 1 
-    else:
-      unsolved.append(i)
-    counter += 1      
+ ##     if final:
+  #     final.print_path()   
+  #     solved += 1 
+  #   else:
+  #     unsolved.append(i)
+  #   counter += 1      
 
-  if counter > 0:  
-    percent = (solved/counter)*100   
-      
-  print("*************************************")  
-  print("{} of {} problems ({} %) solved in less than {} seconds.".format(solved, counter, percent, timebound))  
-  print("Problems that remain unsolved in the set are Problems: {}".format(unsolved))      
-  print("*************************************") 
+ ##   if counter > 0:  
+  #   percent = (solved/counter)*100   
+  #     
+  # print("*************************************")  
+  # print("{} of {} problems ({} %) solved in less than {} seconds.".format(solved, counter, percent, timebound))  
+  # print("Problems that remain unsolved in the set are Problems: {}".format(unsolved))      
+  # print("*************************************") 
 
 
 
